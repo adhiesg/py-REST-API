0555f8228606c2d5b87b18f39987b585af1282c2
adhie <adhie.nugroho@pocket.co.id>
2024-01-28 12:37:17 +0700

    init users database

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+# main.py
+
 from fastapi import FastAPI
+from databases import Database
+
+DATABASE_URL = "sqlite:///./users.db"
+
+database = Database(DATABASE_URL)
 
 app = FastAPI()
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+    print("Connected to the users database")
 
-@app.get("/")
-def read_root():
-    return {"test1": "test1"}
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+    print("Disconnected from the users database")
